7bcfb0790300fd9e8fc83c1e0324fa06440f5e99
Naim Goksel Karacayli <ngokselk@gmail.com>
2023-08-15 19:42:51 -0400

    calibration forest is max key t3

--- src/desi_y1_p1d/ohio_jobs.py
+++ src/desi_y1_p1d/ohio_jobs.py
@@ -764,8 +764,8 @@ class DataJobChain(JobChain):
                     sysopt=None, settings=settings, section=f"qmle.{forest}",
                     jobname=f"sq-job-{forest}")
 
-        calib_forests = ['Lya', 'SB1', 'SB2', 'SB3']
-        ckey = max(list(self.qsonic_jobs.keys()))
+        calib_forests = list(self.qsonic_jobs.keys())
+        ckey = max(calib_forests)
         calibfile = f"{self.qsonic_jobs[ckey].outdelta_dir}/attributes.fits"
         for cf in calib_forests:
             forest = f"{cf}Calib"
