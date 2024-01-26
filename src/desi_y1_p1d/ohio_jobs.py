ff3fe9654cfc38a1691fdf4192f6a450b55d0216
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-01-26 17:26:24 -0500

    fix cat when no errorlog exists

--- src/desi_y1_p1d/ohio_jobs.py
+++ src/desi_y1_p1d/ohio_jobs.py
@@ -595,8 +595,9 @@ class QmleJob(LyspeqJob):
 
         # remove error_logs
         script_txt += utils.get_script_text_for_master_node(
-            "cat results/error_log*.txt > results/error_logs_all.txt && "
-            "rm results/error_log*.txt")
+            "if [ $(ls -1 results/error_log*.txt 2>/dev/null | wc -l) -gt 0 ]; "
+            "then cat results/error_log*.txt > results/error_logs_all.txt && "
+            "rm results/error_log*.txt; fi")
 
         self.submitter_fname = utils.save_submit_script(
             script_txt, self.working_dir, self.jobname)
