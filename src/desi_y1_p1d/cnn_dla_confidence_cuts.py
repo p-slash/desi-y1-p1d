5326de74b1d50abca9fa1ca5bfc92b73b1dac674
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-04-24 17:37:00 +0300

    GP selection and ZDLA cuts

--- src/desi_y1_p1d/cnn_dla_confidence_cuts.py
+++ src/desi_y1_p1d/cnn_dla_confidence_cuts.py
@@ -3,17 +3,22 @@ import fitsio
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("DLA_CAT", help="DLA catalogs")
 
     parser.add_argument(
         "--cnn-conf-high", help="cnn confidence cut for SNR>cnn-snr-divide",
         default=0., type=float)
     parser.add_argument(
-        "--cnn-conf-low", help="cnn confidence cut SNR<cnn-snr-divide", default=0.3, type=float)
-    parser.add_argument("--cnn-snr-divide", help="cnn snr threshold", default=3.0, type=float)
-    # parser.add_argument("--gp-conf", help="gp confidence cut", default=0.9, type=float)
-    parser.add_argument("--nhi", help="DLA column density", default=20.3, type=float)
+        "--cnn-conf-low", help="cnn confidence cut SNR<cnn-snr-divide",
+        default=0.3, type=float)
+    parser.add_argument(
+        "--cnn-snr-divide", help="cnn snr threshold", default=3.0, type=float)
+    parser.add_argument(
+        "--gp-conf", help="gp confidence cut", default=0, type=float)
+    parser.add_argument(
+        "--nhi", help="DLA column density", default=20.3, type=float)
 
     return parser
 
@@ -29,17 +34,13 @@ def get_fname_dla_base(fname):
     return fname[:i]
 
 
-def main():
-    args = get_parser().parse_args()
-
-    dla_cat = fitsio.FITS(args.DLA_CAT)[1].read()
-
-    wnhi = dla_cat['NHI'] > args.nhi
-
+def cnn_selection(dla_cat, args):
     if "CNN_DLA_CONFIDENCE" in dla_cat.dtype.names:
         cnn_conf_dname = "CNN_DLA_CONFIDENCE"
-    else:
+    elif "DLA_CONFIDENCE" in dla_cat.dtype.names:
         cnn_conf_dname = "DLA_CONFIDENCE"
+    else:
+        return True
 
     whigh = (
         (dla_cat[cnn_conf_dname] > args.cnn_conf_high)
@@ -49,7 +50,31 @@ def main():
         (dla_cat[cnn_conf_dname] > args.cnn_conf_low)
         & (dla_cat['S2N'] <= args.cnn_snr_divide)
     )
-    wall = wnhi & (whigh | wlow)
+
+    return whigh | wlow
+
+
+def gp_selection(dla_cat, args):
+    if "GP_DLA_CONFIDENCE" not in dla_cat.dtype.names:
+        return True
+
+    return dla_cat["GP_DLA_CONFIDENCE"] > args.gp_conf
+
+
+def main():
+    args = get_parser().parse_args()
+
+    dla_cat = fitsio.FITS(args.DLA_CAT)[1].read()
+
+    lycont_lim = 1215.67 / 910.
+    wzdla = (
+        (dla_cat['Z_DLA'] < dla_cat['Z_QSO'])
+        & ((lycont_lim * (1 + dla_cat['Z_DLA'])) > (1 + dla_cat['Z_QSO']))
+    )
+    wnhi = dla_cat['NHI'] > args.nhi
+
+    wall = ((cnn_selection(dla_cat, args) | gp_selection(dla_cat, args))
+            & wnhi & wzdla)
 
     final_dla_catalog = dla_cat[wall]
 
@@ -59,7 +84,9 @@ def main():
     fname = (f"{fname_dla_base}"
              f"-nhi{args.nhi:.1f}-cnnSNR{args.cnn_snr_divide:.1f}"
              f"-highcut{args.cnn_conf_high:.1f}"
-             f"-lowcut{args.cnn_conf_low:.1f}.fits")
+             f"-lowcut{args.cnn_conf_low:.1f}"
+             f"-gpconf{args.gp_conf:.1f}.fits")
 
+    print(f"Saved as {fname}")
     with fitsio.FITS(fname, 'rw', clobber=True) as fts:
         fts.write(final_dla_catalog, extname='DLACAT')
